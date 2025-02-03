2c262d92079e48fb16b2509b963a8143587a08ca
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-02-03 02:08:15 -0300

    feat: CommandLineRunner implements

--- src/main/java/com/br/study/movies/MoviesApplication.java
+++ src/main/java/com/br/study/movies/MoviesApplication.java
@@ -1,13 +1,18 @@
 package com.br.study.movies;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoviesApplication {
+public class MoviesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("end of class");
+	}
 }
